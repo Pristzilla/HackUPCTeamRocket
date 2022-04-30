13c54fcd908ba3a8646e7e7864cc697e4bf63b5c
Mrbid67 <skyvidas@gmail.com>
2022-04-30 23:23:34 +0200

    save function implemented

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from queue import Empty
 import sys
 
 from PyQt5 import QtWidgets, QtGui
@@ -11,11 +12,24 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
 
+global spammers
 
 def okClicked():
-	msg = QMessageBox()
-	msg.setWindowTitle("Pressed Ok")
-	msg.exec()
+
+	global spammers
+	
+
+	if(len(spammers) != 0):
+		msg = QMessageBox()
+		msg.setWindowIcon(QtGui.QIcon('save.png'))
+		msg.setWindowTitle("Saved")
+		msg.setText("Results Saved succesfully in file -> potentialSpammers.txt")
+		msg.exec()
+		with open('potentialSpammers.txt','w') as f:
+			for line in spammers.keys():
+				f.write(line)
+				f.write('\n')
+		f.close()
 	
 	
 
@@ -28,24 +42,28 @@ def initializeWindow():
 	window.pushButton_2.clicked.connect(okClicked)
 	window.pushButton.clicked.connect(closeapp)
 	window.pushButton_3.clicked.connect(getUrl)
+	print("Window initialized")
 
 
 
 
 def getUrl():
+	global spammers
 	url = window.lineEdit.text()
 	print(url)
-	spamPredictor.loadComments(url, window.spinBox.value(), window.spinBox_2.value())
-	spammers = spamPredictor.getSuspectedSpammers()
-	window.tableWidget.setRowCount(spammers.__len__())
+	if(url != ""):
+		spamPredictor.loadComments(url, window.spinBox.value(), window.spinBox_2.value())
+		spammers = spamPredictor.getSuspectedSpammers()
+		print("Spammers succesfully identyfied")
+		window.tableWidget.setRowCount(spammers.__len__())
 
-	i = 0
-
-	for key,element in spammers.items():
+		i = 0
 		
-		window.tableWidget.setItem(i,0, QtWidgets.QTableWidgetItem(key))
-		window.tableWidget.setItem(i,1, QtWidgets.QTableWidgetItem(element))
-		i+=1
+		for key,element in spammers.items():
+			
+			window.tableWidget.setItem(i,0, QtWidgets.QTableWidgetItem(key))
+			window.tableWidget.setItem(i,1, QtWidgets.QTableWidgetItem(element))
+			i+=1
 
 
 
